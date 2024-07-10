86e73bfe0b09fcf09b844a2d5670b705240971c3
gaulerie <emilien.courvoisier@gmail.com>
2024-07-10 19:23:04 +0900

    Final

--- beta/assets/js/script.js
+++ beta/assets/js/script.js
@@ -1,22 +1,25 @@
-$(document).ready(function () {
-    let currentVinIndex = 0;
-    const vins = $('.vin-item');
-    const totalVins = vins.length;
-
+$(document).ready(function() {
+    // Implementing the carousel functionality for vins section
+    let currentIndex = 0;
+    const vinItems = $('.vin-item');
+    const totalItems = vinItems.length;
+  
     function showVin(index) {
-        vins.hide();
-        $(vins[index]).show();
+      vinItems.hide();
+      $(vinItems[index]).show();
     }
-
-    $('#prev-vin').click(function () {
-        currentVinIndex = (currentVinIndex - 1 + totalVins) % totalVins;
-        showVin(currentVinIndex);
+  
+    $('#prev-vin').click(function() {
+      currentIndex = (currentIndex > 0) ? currentIndex - 1 : totalItems - 1;
+      showVin(currentIndex);
     });
-
-    $('#next-vin').click(function () {
-        currentVinIndex = (currentVinIndex + 1) % totalVins;
-        showVin(currentVinIndex);
+  
+    $('#next-vin').click(function() {
+      currentIndex = (currentIndex < totalItems - 1) ? currentIndex + 1 : 0;
+      showVin(currentIndex);
     });
-
-    showVin(currentVinIndex);
-});
+  
+    // Initialize carousel
+    showVin(currentIndex);
+  });
+  
